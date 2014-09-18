a1d34a722712178a09bc7a02e99e18470b9d3b27
Bien Justin Mandac <justinmandac@gmail.com>
2014-09-18 19:59:41 +0800

    Basic functionality.

--- tools/conn_module.js
+++ tools/conn_module.js
@@ -7,5 +7,28 @@ function ConnectionObject (params) {
 }
 
 ConnectionObject.prototype.execute = function (query) {
+    var connection;
+    if  (settings == null) {
+        return;
+    }
+    connection = mysql.createConnection(settings);
+    connection.connect(function (err) {
+        if (err) {
+            throw err;
+        }
+        console.log('[' + connection.threadId + ']Connected to ' + settings.host);
 
+    });
+    connection.query(query, function (err, row, fields) {
+        if (err) {
+            throw err;
+        }
+        console.log(row);
+    });
+    connection.end(function (err) {
+        if (err) {
+            throw err;
+        }
+        console.log('[' + connection.threadId + ']Connection terminated from ' + settings.host);
+    });
 }
