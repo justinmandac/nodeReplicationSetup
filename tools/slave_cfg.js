de8822f822e1b6df263c9e7d17056ffed948af32
Bien Justin Mandac <justinmandac@gmail.com>
2014-09-18 20:50:16 +0800

    Started on using ps-like query.

--- tools/slave_cfg.js
+++ tools/slave_cfg.js
@@ -3,16 +3,19 @@ var settings       = JSON.parse(process.argv[2]),
     mysql          = require('mysql'),
     tools          = require('../tools/utils'),
     connection     = mysql.createConnection(settings.connection_settings),
-    master_opts    = settings.master_settings,
-    m_host         = 'master_host='      + tools.enc(master_opts.host),
-    m_port         = 'master_port='      + master_opts.port,
-    m_user         = 'master_user='      + tools.enc(master_opts.user),
-    m_pwd          = 'master_password="' + master_opts.pw + '"', //passwords have to be enclosed in double quotes
-    m_log          = 'master_log_file='  + tools.enc(master_opts.log),
-    m_pos          = 'master_log_pos='   + master_opts.pos,
+    opts           = settings.master_settings,
+    m_host         = 'master_host='      + tools.enc(opts.host),
+    m_port         = 'master_port='      + opts.port,
+    m_user         = 'master_user='      + tools.enc(opts.user),
+    m_pwd          = 'master_password="' + opts.pw + '"', //passwords have to be enclosed in double quotes
+    m_log          = 'master_log_file='  + tools.enc(opts.log),
+    m_pos          = 'master_log_pos='   + opts.pos,
     master_set_str = 'CHANGE MASTER TO ' + m_host + ',' + m_port + ',' + m_user + ',' + m_pwd + ',' + m_log + ',' + m_pos + ';',
     testQuery      = "SHOW DATABASES;",
     query          = master_set_str;
+//var new_query   = 'CHANGE MASTER TO master_host=?,master_port=?,master_user=?,master_password="?",master_log_file=?,master_log_pos=?;';
+var new_query   = 'CHANGE MASTER TO master_host=?,master_port=?;';
+var params      = [opts.host,opts.port];
 
 connection.connect(function (err) {
     if (err) {
