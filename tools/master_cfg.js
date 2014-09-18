07e9d84096d8f520b5886878718e90ac61d9144b
Bien Justin Mandac <justinmandac@gmail.com>
2014-09-19 00:26:18 +0800

    Used conn_module.

--- tools/master_cfg.js
+++ tools/master_cfg.js
@@ -1,31 +1,10 @@
 //master setup
 var settings       = JSON.parse(process.argv[2]);
-var mysql          = require('mysql');
-var tools          = require('../tools/utils');
-var connection     = mysql.createConnection(settings.connection_settings);
+var conn_module    = require('./conn_module');
+var sql_connect    = new conn_module(settings.connection_settings);
 var testQuery      = 'SHOW DATABASES;';
 var userQuery      = 'GRANT REPLICATION SLAVE ON \'?\'@? IDENTIFIED BY "?";';
+var params         = [];
 var query          = testQuery;
 
-connection.connect(function (err) {
-    if (err) {
-        console.err('Error connecting:' + err.stack);
-        return;
-    }
-
-    console.log('[' + connection.threadId + ']Connected to ' + settings.connection_settings.user + '@' + settings.connection_settings.host);
-
-});
-
-connection.query(query, function (err, rows, fields) {
-    if (err) {
-        throw err;
-    }
-
-    console.log(rows);
-
-});
-
-connection.end(function (err) {
-    console.log('[' + connection.threadId + ']Connection terminated from ' + settings.connection_settings.user + '@' + settings.connection_settings.host);
-});
+sql_connect.execute(query,params);
