69106b1325f16f79baa60ce9ec5df38e14b154c2
Bien Justin Mandac <justinmandac@gmail.com>
2014-09-18 20:22:25 +0800

    Cleanup

--- conn_main.js
+++ conn_main.js
@@ -1,8 +1,7 @@
-var settings = require('./cfg/host_conn_opts.json');
+var settings    = require('./cfg/host_conn_opts.json');
 var conn_module = require('./tools/conn_module');
 var connection  = new conn_module(settings);
 var testQuery   = 'SHOW DATABASES;';
 var query       = testQuery;
 
-//console.log(settings);
 connection.execute(query);
